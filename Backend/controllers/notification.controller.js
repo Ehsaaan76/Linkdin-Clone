bfd98a0a7f3ac4d68f25477694bc2954c9d2ca08
Ehsan ullah <emehsaan.76@gmail.com>
2025-05-07 22:42:04 +0500

    Notification Controller and Routes Added

--- Backend/controllers/notification.controller.js
+++ Backend/controllers/notification.controller.js
@@ -14,6 +14,31 @@ export const getUserNotifications = async (req, res) => {
   }
 };
 
-export const markNotificationAsRead = async (req, res) => {};
+export const markNotificationAsRead = async (req, res) => {
+  const notificationId = req.params.id;
+  try {
+    const notification = await Notification.findByIdAndUpdate(
+      {_id: notificationId, recipient: res.user._id},
+      {read: true},
+      {new: true}
+    );
 
-export const deleteNotification = async (req, res) => {};
+    res.status(200).json(notification)
+  } catch (error) {
+    console.log("Error in markNotificationAsRead: ", error);
+    res.status(500).json({ message: "Internal Server error" });
+  }
+};
+
+export const deleteNotification = async (req, res) => {
+  const notificationId = req.params.id;
+  try {
+    await Notification.findOneAndDelete(
+      {_id: notificationId, recipient: req.user._id}
+    );
+    res.json({ message: "Notification delete successfully" })
+  } catch (error) {
+    console.log("Error in deleteNotification: ", error);
+    res.status(200).json({ message: "Internal Server error" })
+  }
+};
