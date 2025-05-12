9f18966dd04f5f6868bb6c9ba0a4cf1784d4e0ce
Ehsan ullah <emehsaan.76@gmail.com>
2025-05-12 23:47:43 +0500

    Sign in & Sign up Added

--- Frontend/src/components/auth/LoginForm.jsx
+++ Frontend/src/components/auth/LoginForm.jsx
@@ -1,33 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { axiosInstance } from "./../../config/axios";
+import { toast } from "react-hot-toast";
+import { Loader } from 'lucide-react';
 
 function LoginForm() {
-
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
+  const queryClient = useQueryClient();
 
-  }
+  const { mutate: loginMutation, isLoading } = useMutation({
+    mutationFn: (data) => axiosInstance.post("/auth/login", data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["authuser"] });
+      toast.success("Login successful");
+    },
+    onError: (error) => {
+      toast.error(error.response.data.message || "Something went wrong");
+    },
+  });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    loginMutation({ username, password });
+  };
 
   return (
-    <form action="" onSubmit={handleLogin} className='flex flex-col gap-4'>
-      <input type="text" 
-      placeholder='Username'
-      value={username}
-      onChange={(e) => setUsername(e.target.value)}
-      className='input input-bordered w-full bg-gray-100'
-      required/>
-
-      <input type="text" 
-      placeholder='Password'
-      value={password}
-      onChange={(e) => setPassword(e.target.value)}
-      className='input input-bordered w-full bg-gray-100'
-      required
+    <form action="" onSubmit={handleLogin} className="flex flex-col gap-4">
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        className="input input-bordered w-full bg-gray-100"
+        required
       />
-      <button>Login</button>
+
+      <input
+        type="text"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="input input-bordered w-full bg-gray-100"
+        required
+      />
+      <button type="submit" className="w-full btn btn-primary">
+        {isLoading ? <Loader className="size-5 animate-spin" /> : "Sign in" }
+      </button>
     </form>
-  )
+  );
 }
 
-export default LoginForm
+export default LoginForm;
